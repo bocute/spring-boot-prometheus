0edc54b21d731e656fcc0664c0ef3fb914f90309
Rafael Bocute <bocute@gmail.com>
2023-09-02 12:47:52 -0300

    Initial

---
+++ src/test/java/br/com/monitoring/prometheus/monitoringprometheus/MonitoringPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monitoring.prometheus.monitoringprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
